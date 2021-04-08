035c09b9a56d37d5213c3f4eb25365eff3a491b1
Donlon <idonlon@hotmail.com>
2021-04-08 23:20:42 +0800

    add BSP files

---
+++ bsp/standalone_bsp_0/CORTEXM3_AXI_0/include/xparameters.h
@@ -0,0 +1,196 @@
+#ifndef XPARAMETERS_H   /* prevent circular inclusions */
+#define XPARAMETERS_H   /* by using protection macros */
+
+/* Definition for CPU ID */
+#define XPAR_CPU_ID 0U
+
+/* Definitions for peripheral CORTEXM3_AXI_0 */
+#define XPAR_CORTEXM3_AXI_0_CPU_CLK_FREQ_HZ 0
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral CORTEXM3_AXI_0 */
+#define XPAR_CPU_CORTEXM3_0_CPU_CLK_FREQ_HZ 0
+
+
+/******************************************************************/
+
+
+/******************************************************************/
+
+/* Platform specific definitions */
+#define PLATFORM_ARM
+ 
+/* Definitions for sleep timer configuration */
+#define XSLEEP_TIMER_IS_DEFAULT_TIMER
+ 
+ 
+/******************************************************************/
+/* Definitions for driver BRAM */
+#define XPAR_XBRAM_NUM_INSTANCES 2U
+
+/* Definitions for peripheral AXI_BRAM_CTRL_0 */
+#define XPAR_AXI_BRAM_CTRL_0_DEVICE_ID 0U
+#define XPAR_AXI_BRAM_CTRL_0_DATA_WIDTH 32U
+#define XPAR_AXI_BRAM_CTRL_0_ECC 0U
+#define XPAR_AXI_BRAM_CTRL_0_FAULT_INJECT 0U
+#define XPAR_AXI_BRAM_CTRL_0_CE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_UE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_STATUS_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_CE_COUNTER_WIDTH 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_ONOFF_REGISTER 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_AXI_BRAM_CTRL_0_WRITE_ACCESS 0U
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR 0x60000000U
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_HIGHADDR 0x60001FFFU
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_CTRL_HIGHADDR 0xFFFFFFFFU  
+
+
+/* Definitions for peripheral AXI_BRAM_CTRL_1 */
+#define XPAR_AXI_BRAM_CTRL_1_DEVICE_ID 1U
+#define XPAR_AXI_BRAM_CTRL_1_DATA_WIDTH 32U
+#define XPAR_AXI_BRAM_CTRL_1_ECC 0U
+#define XPAR_AXI_BRAM_CTRL_1_FAULT_INJECT 0U
+#define XPAR_AXI_BRAM_CTRL_1_CE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_1_UE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_1_ECC_STATUS_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_1_CE_COUNTER_WIDTH 0U
+#define XPAR_AXI_BRAM_CTRL_1_ECC_ONOFF_REGISTER 0U
+#define XPAR_AXI_BRAM_CTRL_1_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_AXI_BRAM_CTRL_1_WRITE_ACCESS 0U
+#define XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR 0x02000000U
+#define XPAR_AXI_BRAM_CTRL_1_S_AXI_HIGHADDR 0x02001FFFU
+#define XPAR_AXI_BRAM_CTRL_1_S_AXI_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_AXI_BRAM_CTRL_1_S_AXI_CTRL_HIGHADDR 0xFFFFFFFFU  
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral AXI_BRAM_CTRL_0 */
+#define XPAR_BRAM_0_DEVICE_ID XPAR_AXI_BRAM_CTRL_0_DEVICE_ID
+#define XPAR_BRAM_0_DATA_WIDTH 32U
+#define XPAR_BRAM_0_ECC 0U
+#define XPAR_BRAM_0_FAULT_INJECT 0U
+#define XPAR_BRAM_0_CE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_0_UE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_0_ECC_STATUS_REGISTERS 0U
+#define XPAR_BRAM_0_CE_COUNTER_WIDTH 0U
+#define XPAR_BRAM_0_ECC_ONOFF_REGISTER 0U
+#define XPAR_BRAM_0_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_BRAM_0_WRITE_ACCESS 0U
+#define XPAR_BRAM_0_BASEADDR 0x60000000U
+#define XPAR_BRAM_0_HIGHADDR 0x60001FFFU
+#define XPAR_BRAM_0_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_BRAM_0_CTRL_HIGHADDR 0xFFFFFFFEU  
+
+/* Canonical definitions for peripheral AXI_BRAM_CTRL_1 */
+#define XPAR_BRAM_1_DEVICE_ID XPAR_AXI_BRAM_CTRL_1_DEVICE_ID
+#define XPAR_BRAM_1_DATA_WIDTH 32U
+#define XPAR_BRAM_1_ECC 0U
+#define XPAR_BRAM_1_FAULT_INJECT 0U
+#define XPAR_BRAM_1_CE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_1_UE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_1_ECC_STATUS_REGISTERS 0U
+#define XPAR_BRAM_1_CE_COUNTER_WIDTH 0U
+#define XPAR_BRAM_1_ECC_ONOFF_REGISTER 0U
+#define XPAR_BRAM_1_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_BRAM_1_WRITE_ACCESS 0U
+#define XPAR_BRAM_1_BASEADDR 0x02000000U
+#define XPAR_BRAM_1_HIGHADDR 0x02001FFFU
+#define XPAR_BRAM_1_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_BRAM_1_CTRL_HIGHADDR 0xFFFFFFFEU  
+
+
+/******************************************************************/
+
+/* Definitions for driver GPIO */
+#define XPAR_XGPIO_NUM_INSTANCES 1
+
+/* Definitions for peripheral AXI_GPIO_0 */
+#define XPAR_AXI_GPIO_0_BASEADDR 0x40100000
+#define XPAR_AXI_GPIO_0_HIGHADDR 0x4010FFFF
+#define XPAR_AXI_GPIO_0_DEVICE_ID 0
+#define XPAR_AXI_GPIO_0_INTERRUPT_PRESENT 0
+#define XPAR_AXI_GPIO_0_IS_DUAL 0
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral AXI_GPIO_0 */
+#define XPAR_GPIO_0_BASEADDR 0x40100000
+#define XPAR_GPIO_0_HIGHADDR 0x4010FFFF
+#define XPAR_GPIO_0_DEVICE_ID XPAR_AXI_GPIO_0_DEVICE_ID
+#define XPAR_GPIO_0_INTERRUPT_PRESENT 0
+#define XPAR_GPIO_0_IS_DUAL 0
+
+
+/******************************************************************/
+
+/* Definitions for driver SPI */
+#define XPAR_XSPI_NUM_INSTANCES 1U
+
+/* Definitions for peripheral AXI_QUAD_SPI_0 */
+#define XPAR_AXI_QUAD_SPI_0_DEVICE_ID 0U
+#define XPAR_AXI_QUAD_SPI_0_BASEADDR 0x40120000U
+#define XPAR_AXI_QUAD_SPI_0_HIGHADDR 0x4012FFFFU
+#define XPAR_AXI_QUAD_SPI_0_FIFO_DEPTH 16U
+#define XPAR_AXI_QUAD_SPI_0_FIFO_EXIST 1U
+#define XPAR_AXI_QUAD_SPI_0_SPI_SLAVE_ONLY 0U
+#define XPAR_AXI_QUAD_SPI_0_NUM_SS_BITS 1U
+#define XPAR_AXI_QUAD_SPI_0_NUM_TRANSFER_BITS 8U
+#define XPAR_AXI_QUAD_SPI_0_SPI_MODE 2U
+#define XPAR_AXI_QUAD_SPI_0_TYPE_OF_AXI4_INTERFACE 1U
+#define XPAR_AXI_QUAD_SPI_0_AXI4_BASEADDR 0x01000000U
+#define XPAR_AXI_QUAD_SPI_0_AXI4_HIGHADDR 0x010FFFFFU
+#define XPAR_AXI_QUAD_SPI_0_XIP_MODE 1U
+
+/* Canonical definitions for peripheral AXI_QUAD_SPI_0 */
+#define XPAR_SPI_0_DEVICE_ID 0U
+#define XPAR_SPI_0_BASEADDR 0x40120000U
+#define XPAR_SPI_0_HIGHADDR 0x4012FFFFU
+#define XPAR_SPI_0_FIFO_DEPTH 16U
+#define XPAR_SPI_0_FIFO_EXIST 1U
+#define XPAR_SPI_0_SPI_SLAVE_ONLY 0U
+#define XPAR_SPI_0_NUM_SS_BITS 1U
+#define XPAR_SPI_0_NUM_TRANSFER_BITS 8U
+#define XPAR_SPI_0_SPI_MODE 2U
+#define XPAR_SPI_0_TYPE_OF_AXI4_INTERFACE 1U
+#define XPAR_SPI_0_AXI4_BASEADDR 0x01000000U
+#define XPAR_SPI_0_AXI4_HIGHADDR 0x010FFFFFU
+#define XPAR_SPI_0_XIP_MODE 1U
+#define XPAR_SPI_0_USE_STARTUP 0U
+
+
+
+/******************************************************************/
+
+/* Definitions for driver UARTLITE */
+#define XPAR_XUARTLITE_NUM_INSTANCES 1
+
+/* Definitions for peripheral AXI_UARTLITE_0 */
+#define XPAR_AXI_UARTLITE_0_BASEADDR 0x40110000
+#define XPAR_AXI_UARTLITE_0_HIGHADDR 0x4011FFFF
+#define XPAR_AXI_UARTLITE_0_DEVICE_ID 0
+#define XPAR_AXI_UARTLITE_0_BAUDRATE 9600
+#define XPAR_AXI_UARTLITE_0_USE_PARITY 0
+#define XPAR_AXI_UARTLITE_0_ODD_PARITY 0
+#define XPAR_AXI_UARTLITE_0_DATA_BITS 8
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral AXI_UARTLITE_0 */
+#define XPAR_UARTLITE_0_DEVICE_ID XPAR_AXI_UARTLITE_0_DEVICE_ID
+#define XPAR_UARTLITE_0_BASEADDR 0x40110000
+#define XPAR_UARTLITE_0_HIGHADDR 0x4011FFFF
+#define XPAR_UARTLITE_0_BAUDRATE 9600
+#define XPAR_UARTLITE_0_USE_PARITY 0
+#define XPAR_UARTLITE_0_ODD_PARITY 0
+#define XPAR_UARTLITE_0_DATA_BITS 8
+
+
+/******************************************************************/
+
+#endif  /* end of protection macro */
--- main.c
+++ main.c
@@ -1,40 +1,75 @@
+#include <stdio.h>
+
 #include <cm3soc/cm3soc.h>
 
 #include <xparameters.h>
 #include <xgpio.h>
+#include <xuartlite.h>
 
 int value = 1;
 
 XGpio gpio;   /* The driver instance for GPIO Device 0 */
+XUartLite uart_inst;         /* Instance of the UartLite device */
 
 int systick_init() {
     return SysTick_Config(50e6 / 4); // 4 Hz
 }
 
-int system_init() {
-    int status;
-    status = systick_init();
+int gpio_init() {
+    int status = XGpio_Initialize(&gpio, XPAR_AXI_GPIO_0_DEVICE_ID); // initialize GPIO
     if (status != XST_SUCCESS) {
         return status;
     }
-    // xil_printf("Hello, World!\n");
-
-    /*
-     * Initialize the GPIO driver so that it's ready to use,
-     * specify the device ID that is generated in xparameters.h
-    */
-    status = XGpio_Initialize(&gpio, XPAR_AXI_GPIO_0_DEVICE_ID);
-    if (status != XST_SUCCESS) {
-        return status;
-    }
-    XGpio_SetDataDirection(&gpio, 1, 0);
-    XGpio_DiscreteWrite(&gpio, 1, 0x2);
+    XGpio_SetDataDirection(&gpio, 1, 0); // set GPIO direction
+    XGpio_DiscreteWrite(&gpio, 1, 0x2); // set GPIO output
 
     return status;
 }
 
+int uart_init() {
+    /*
+     * Initialize the UartLite driver so that it's ready to use.
+     */
+    int status = XUartLite_Initialize(&uart_inst, XPAR_UARTLITE_0_DEVICE_ID);
+    if (status != XST_SUCCESS) {
+        return XST_FAILURE;
+    }
+
+    /*
+     * Perform a self-test to ensure that the hardware was built correctly.
+     */
+    status = XUartLite_SelfTest(&uart_inst);
+    if (status != XST_SUCCESS) {
+        return XST_FAILURE;
+    }
+    return status;
+}
+
+int system_init() {
+    int status;
+    status = uart_init();
+    if (status != XST_SUCCESS) {
+        return status;
+    }
+    status = systick_init();
+    if (status != XST_SUCCESS) {
+        return status;
+    }
+    status = gpio_init();
+    if (status != XST_SUCCESS) {
+        return status;
+    }
+    // xil_printf("Hello, World!\n");
+    return status;
+}
+
 int main() {
-    system_init();
+    int status = system_init();
+    if (status != XST_SUCCESS) {
+        while (1) {
+        }
+    }
+
     return 0;
 }
 
@@ -43,6 +78,13 @@ void SysTick_Handler() {
         XGpio_DiscreteWrite(&gpio, 1, 0b01);
     } else {
         XGpio_DiscreteWrite(&gpio, 1, 0b10);
+    }
+    if (value % 4 == 0) {
+        char str_buffer[64];
+        int len = snprintf(str_buffer, sizeof(str_buffer),
+                           "Hello from Cortex-M3 soft-core processor, t=%ds\r\n", value / 4);
+        XUartLite_Send(&uart_inst, (u8 *) str_buffer, len);
+        xil_printf("Hello from Cortex-M3 soft-core processor, t=%ds\r\n", value / 4);
     }
     value++;
 }
